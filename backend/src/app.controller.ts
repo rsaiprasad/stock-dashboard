cbd650c4c69b9ce982d48ca7d2eb185d5b841df4
Sai Prasad Rakasi <saiprasa@amazon.com>
2025-04-29 19:14:34 -0700

    Added health check for the backend container as dependency to start frontend container

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  @HttpCode(200)
+  health() {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString()
+    };
   }
 }
